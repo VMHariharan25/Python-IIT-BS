9987dc3ff357ef7837828ff4789e9d2bc2d85e57
Hariharan Velmurugan <hariharanvels@velsinfoway.com>
2025-06-09 22:15:10 +0530

    June 9 Class

---
+++ Basics of Python/BasicOperations.py
@@ -0,0 +1,35 @@
+# June 9, 2025
+# Addition of Three Numbers 
+print(5+2+3)
+
+''' 
+    Execution of complex Expression 
+    for understanding 
+'''
+print(5 + 2 * 3 - 4 / 2)
+
+"""
+>>> x = 5.6
+>>> type(x)
+<class 'float'>
+
+>>> y = '5.6'
+>>> type(y)
+<class 'str'>
+
+
+"""
+
+"""
+IMPORTANT NOTE: INDENTATION IS CRUCIAL IN PYTHON
+# Indentation Example
+>>>  x = 1
+  File "<stdin>", line 1
+    x = 1
+IndentationError: unexpected indent
+
+"""
+
+x = 5 
+print(type(x)) 
+
